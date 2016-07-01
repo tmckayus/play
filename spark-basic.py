ce156aaa22f0d29d6fb28f82ac2af053ef01a111
Trevor McKay <tmckay@redhat.com>
2016-07-01 15:07:02 -0400

    first commit

---
+++ spark-basic.py
@@ -0,0 +1,15 @@
+# spark-basic.py
+from pyspark import SparkConf
+from pyspark import SparkContext
+
+conf = SparkConf()
+conf.setAppName('spark-basic')
+sc = SparkContext(conf=conf)
+
+def mod(x):
+#    import numpy as np
+#    return (x, np.mod(x, 2))
+    return (x, x % 2)
+
+rdd = sc.parallelize(range(1000)).map(mod).take(10)
+print rdd
