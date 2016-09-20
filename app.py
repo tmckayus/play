acb9859ca1c6897b1c1ab696d966487de7702340
Trevor McKay <tmckay@redhat.com>
2016-09-20 11:49:01 -0400

    Path experiment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # spark-basic.py
+import sys
 from pyspark import SparkConf
 from pyspark import SparkContext
 
@@ -10,5 +11,6 @@ def mod(x):
     return (x, x % 2)
 
 print "this is me!"
+print('\n'.join(sys.path))
 rdd = sc.parallelize(range(1000)).map(mod).take(30)
 print rdd
