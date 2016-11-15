e57fc172e061ed700041216b40823b77c6d72f95
Trevor McKay <tmckay@redhat.com>
2016-11-15 09:24:17 -0500

    remove test code

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ sc = SparkContext(conf=conf)
 def mod(x):
     return (x, x % 2)
 
-print "this is me!"
 print('\n'.join(sys.path))
 rdd = sc.parallelize(range(1000)).map(mod).take(30)
 print rdd
-print("sleeping for 10 minutes")
-time.sleep(600)
