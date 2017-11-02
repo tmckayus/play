2928f16687483d06207239d247bda3c0f92a0c81
Trevor McKay <tmckay@redhat.com>
2017-11-02 08:59:24 -0400

    Try example version of pi.py

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
-# spark-basic.py
-import sys
-import time
-import numpy
-
-from pyspark import SparkConf
-from pyspark import SparkContext
-
-conf = SparkConf()
-conf.setAppName('spark-basic')
-sc = SparkContext(conf=conf)
-
-def mod(x):
-    return (x, numpy.mod(x, 2))
-
-print('\n'.join(sys.path))
-rdd = sc.parallelize(range(1000000)).map(mod).take(30)
-print rdd
-time.sleep(60)
+from __future__ import print_function
+import sys                                                                                                                                         
+from random import random                                                                                                                          
+from operator import add                                                                                                                           
+                                                                                                                                                   
+from pyspark.sql import SparkSession                                                                                                               
+                                                                                                                                                   
+                                                                                                                                                   
+if __name__ == "__main__":                                                                                                                         
+    """                                                                                                                                            
+        Usage: pi [partitions]                                                                                                                     
+    """                                                                                                                                            
+    spark = SparkSession\                                                                                                                          
+        .builder\                                                                                                                                  
+        .appName("PythonPi")\                                                                                                                      
+        .getOrCreate()                                                                                                                             
+                                                                                                                                                   
+    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2                                                                                      
+    n = 100000 * partitions                                                                                                                        
+                                                                                                                                                   
+    def f(_):                                                                                                                                      
+        x = random() * 2 - 1                                                                                                                       
+        y = random() * 2 - 1                                                                                                                       
+        return 1 if x ** 2 + y ** 2 <= 1 else 0                                                                                                    
+                                                                                                                                                   
+    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)                                                         
+    print("Pi is roughly %f" % (4.0 * count / n))                                                                                                  
+                                                                                                                                                   
+    spark.stop()
